fe3fe3bb5fd754e3619f08be253059606019c504
joel <joel.oliveira@gmail.com>
2010-10-17 15:11:07 -0400

    memoized helper methods for messageboard and topic. attr_accessible user and last_user fields in Topic. Before filter for create and update to explicitly set the user and last_user fields.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,30 +1,48 @@
 class TopicsController < ApplicationController
+  before_filter :pad_params, :only => [:create, :update]
+  helper_method :messageboard, :topic
 
   def index
-    @topic =  Topic.all
+    @topics = Topic.all
   end
 
   def show
-    @topic = Topic.where(:name => params[:id]).first
   end
   
   def new
-    @messageboard = Messageboard.where(:name => params[:messageboard_id]).first
     @topic = Topic.new
   end
 
   def create
-    @messageboard = Messageboard.where(:name => params[:messageboard_id]).first
-    @topic = @messageboard.topics.create(params[:topic])
-    redirect_to messageboard_topics_path(@messageboard)
+    @topic = messageboard.topics.create(params[:topic])
+    redirect_to messageboard_topics_path(messageboard)
+  end
+
+  def edit
   end
  
+  def update
+    # TODO
+  end
+
+  # ======================================
+ 
+  def messageboard
+    @messageboard ||= Messageboard.where(:name => params[:messageboard_id]).first
+  end
+
+  def topic
+    @topic ||= Topic.find(params[:id])
+  end
+
+  # ======================================
+
   private
 
     def pad_params
-      params[:topic][:user] = current_user.name
-      params[:topic][:last_user] = current_user.name
+      current_user_name = current_user.nil? ? "anonymous commenter" : current_user.name
+      params[:topic][:user] = current_user_name
+      params[:topic][:last_user] = current_user_name
     end
 
 end
-
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,6 @@
 class Topic
   include Mongoid::Document
   include Mongoid::Timestamps
-  before_create :add_user_info
   
   field :title, :type => String
   field :user, :type => String
@@ -12,17 +11,10 @@ class Topic
   field :categories, :type => Array, :default => []
   field :tags, :type => Array, :default => []
   field :subscribers, :type => Array, :default => []
+  
+  attr_accessible :title, :user, :last_user
 
   referenced_in :messageboard
-
   embeds_many :posts
   accepts_nested_attributes_for :posts
-  
-  attr_accessible :title
-
-  private
-    def add_user_info
-      debugger
-      self.user = self.last_user =  ""
-    end
 end
