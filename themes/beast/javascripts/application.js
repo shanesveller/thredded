21cd58b8443c558a4ad796b2b616a6ddf6efc072
joel <joel.oliveira@gmail.com>
2011-02-25 22:05:34 -0500

    Styling and a sprinkling of JS for the new topic page

--- themes/beast/javascripts/application.js
+++ themes/beast/javascripts/application.js
@@ -1,4 +1,12 @@
 jQuery(document).ready(function() {
   jQuery.timeago.settings.allowFuture = true;
   jQuery("abbr.timeago").timeago();
+
+  jQuery('.handle a').bind('click', function(){
+    
+    jQuery('#attributes').slideDown('fast', function(){
+      return true;
+    });
+    return false;
+  });
 });
