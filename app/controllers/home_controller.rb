1a7768a5f09e47e85dee97298e0d88ed381b17c8
joel <joel.oliveira@gmail.com>
2011-05-17 23:07:09 -0400

    Refining the logic that decides who goes where when they hit the homepage controller.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,29 @@ class HomeController < ApplicationController
   layout 'application'
 
   def index
+    @messageboard = Messageboard.first(:conditions => { :name => THREDDED[:default_messageboard_name] }) 
 
-    if @messageboard = Messageboard.first(:conditions => { :name => THREDDED[:default_messageboard_name] })
-      if THREDDED[:default_messageboard_home] == 'topics' && @messageboard.security == :private && current_user.nil?
-        flash[:alert] = "This messageboard is private. Please log in."
-        redirect_to new_user_session_url
-      elsif THREDDED[:default_messageboard_home] == 'topics' && @messageboard.security == :logged_in && current_user.nil?
-        flash[:alert] = "This messageboard is public, but you must be logged in to see it."
-        redirect_to new_user_session_url
-      elsif THREDDED[:default_messageboard_home] == 'topics' || (@messageboard.security == :logged_in && current_user.present?)
+
+    if @messageboard && @messageboard.default_home_is_topics?
+      # ---------
+      if @messageboard.restricted_to_private? && current_user.nil?
+        flash[:alert] = "This messageboard is private. Please log in." and redirect_to new_user_session_url
+
+      elsif @messageboard.restricted_to_logged_in? && current_user.nil?
+        flash[:alert] = "This messageboard is public, but you must be logged in to see it." and redirect_to new_user_session_url
+
+      elsif @messageboard.restricted_to_private? && current_user.present? && current_user.member_of?(@messageboard)
         redirect_to topics_path(@messageboard)
-      end
-    end
 
+      elsif @messageboard.restricted_to_logged_in? && current_user.present? 
+        redirect_to topics_path(@messageboard)
+
+      elsif @messageboard.public?
+        redirect_to topics_path(@messageboard)
+
+      end
+      # ---------
+    end
   end
+
 end
