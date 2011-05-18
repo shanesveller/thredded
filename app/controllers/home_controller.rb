bf06ab73d280cf4379704730b90356335e7ca23c
joel <joel.oliveira@gmail.com>
2011-05-17 23:15:36 -0400

    Adding todo for home controller to remind me to extract some cruft to the messageboard model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
   def index
     @messageboard = Messageboard.first(:conditions => { :name => THREDDED[:default_messageboard_name] }) 
 
-
+    # TODO: Extract all this out to the Messageboard model
+    #
     if @messageboard && @messageboard.default_home_is_topics?
       # ---------
       if @messageboard.restricted_to_private? && current_user.nil?
