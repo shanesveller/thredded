e076eff10b760becbd8efbf7204beec192e881b9
joel <joel.oliveira@gmail.com>
2011-07-16 12:08:57 -0400

    changed user to ActiveRecord, adding db.yml sample, and deleting mongoid sample yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,42 +1,36 @@
-class User
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatablei, :omniauthable, :token_authenticatable
 
-  field :name, :type => String
-  field :superadmin, :type => Boolean, :default => false
-  field :posts_count, :type => Integer, :default => 0
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 
-  references_and_referenced_in_many :messageboards
-  references_and_referenced_in_many :roles
-  references_and_referenced_in_many :topics
+  # references_and_referenced_in_many :messageboards
+  # references_and_referenced_in_many :roles
+  # references_and_referenced_in_many :topics
   
   validates_presence_of :name
   validates_uniqueness_of :name, :email, :case_sensitive => false
-  attr_accessible :name, :email, :password, :password_confirmation
 
   def superadmin?
     self.superadmin
   end
 
   def admins?(messageboard)
-    superadmin? || self.roles.for(messageboard).as([:admin]).size > 0
+    superadmin? || roles.for(messageboard).as([:admin]).size > 0
   end
 
   def moderates?(messageboard)
-    superadmin? || self.roles.for(messageboard).as([:admin, :moderator]).size > 0
+    superadmin? || roles.for(messageboard).as([:admin, :moderator]).size > 0
   end
 
   def member_of?(messageboard)
-    superadmin? || self.roles.for(messageboard).as([:admin, :moderator, :member]).size > 0
+    superadmin? || roles.for(messageboard).as([:admin, :moderator, :member]).size > 0
   end
   
   def member_of(messageboard, as=:member)
-    roles << Role.create(:level => as, :messageboard => messageboard)
-    self.save
+    roles << Role.create(:level => as, :messageboard => messageboard) and save
   end
 
   def admin_of(messageboard, as=:member)
