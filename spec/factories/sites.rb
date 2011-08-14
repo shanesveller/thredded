3190a921ee809b8c2702bdd44ac03ed64a16ae95
joel <joel.oliveira@gmail.com>
2011-08-14 18:39:31 -0400

    Adding a title and description field to the Site factory

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -2,5 +2,7 @@ Factory.define :site do |f|
   f.slug "thredded"
   f.permission "public"
   f.domain ""
+  f.title "Default website"
+  f.description "default website description"
   f.association :user
 end
