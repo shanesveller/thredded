73732c787a637f5328e834022b0f395c1b67f20e
joel <joel.oliveira@gmail.com>
2011-08-14 18:44:39 -0400

    changing session store from cookie to active record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110810212910) do
+ActiveRecord::Schema.define(:version => 20110814141030) do
 
   create_table "images", :force => true do |t|
     t.integer  "width"
@@ -66,6 +66,16 @@ ActiveRecord::Schema.define(:version => 20110810212910) do
 
   add_index "roles", ["messageboard_id"], :name => "index_roles_on_messageboard_id"
   add_index "roles", ["user_id"], :name => "index_roles_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "sites", :force => true do |t|
     t.integer "user_id"
