3ca975cd267b510e9adffa582028d45dad5c685d
joel <joel.oliveira@gmail.com>
2011-08-14 18:23:15 -0400

    the multi-site steps are all good to go. working and passing

--- features/step_definitions/multi_site_steps.rb
+++ features/step_definitions/multi_site_steps.rb
@@ -1,33 +1,52 @@
 Given /^the default "([^"]*)" website domain is "([^"]*)"$/ do |permission, website|
-  @site = Factory(:site, :domain => website, :permission => permission)
+  THREDDED[:default_domain] = website
+  THREDDED[:default_site]   = website.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  @site = Factory(:site, :slug => THREDDED[:default_site], :domain => THREDDED[:default_domain], :permission => permission)
 end
 
 Given /^I visit "([^"]*)"$/ do |domain|
-  Capybara.default_host = domain #for Rack::Test
-# Capybara.app_host = "http://#{domain}:9887" if Capybara.current_driver == :selenium 
+  slug = domain.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  @site = Site.exists?(:domain => domain) ? Site.find_by_domain(domain) : Site.find_by_slug(slug)
+  visit site_messageboards_path(@site.slug)
 end
 
-Given /^the default website has two messageboards named "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
-  # debugger
+Given /^the default website has two messageboards named "([^"]*)" and "([^"]*)"$/ do |messageboard1, messageboard2|
+  @site = Site.find_by_domain(THREDDED[:default_domain])
+  @site.messageboards << Factory(:messageboard, :name => messageboard1)
+  @site.messageboards << Factory(:messageboard, :name => messageboard2)
+  @site.save
 end
 
-Then /^I should see messageboards "([^"]*)" and "([^"]*)"$/ do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see the messageboard called "([^"]*)"$/ do |messageboard|
+  page.should have_selector(".messageboard header h2", :text => messageboard)
 end
 
-Given /^a subdomain site exists called "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see messageboards "([^"]*)" and "([^"]*)"$/ do |messageboard1, messageboard2|
+  Then %{I should see the messageboard called "#{messageboard1}"}
+  And  %{I should see the messageboard called "#{messageboard2}"}
 end
 
-Given /^"([^"]*)" has two messageboards named "([^"]*)" and "([^"]*)"$/ do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+Given /^a subdomain site exists called "([^"]*)"$/ do |subdomain|
+  slug = subdomain.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  Factory(:site, :slug => slug) 
 end
 
-Given /^a custom domain site exists called "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^"([^"]*)" has two messageboards named "([^"]*)" and "([^"]*)"$/ do |subdomain, messageboard1, messageboard2|
+  slug = subdomain.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  @site = Site.exists?(:domain => subdomain) ? Site.find_by_domain( subdomain ) : Site.find_by_slug( slug )
+  @site.messageboards << Factory(:messageboard, :name => messageboard1)
+  @site.messageboards << Factory(:messageboard, :name => messageboard2)
+  @site.save
+end
+
+Given /^a custom domain site exists called "([^"]*)"$/ do |domain|
+  Factory(:site, :slug => domain.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, ''), :domain => domain)
 end
 
 Then /^I should see the login form$/ do
-  pending # express the regexp above with the code you wish you had
+  page.should have_selector('form#user_new')
 end
 
+Then /^I should not see the login form$/ do
+  page.should_not have_selector('form#user_new')
+end
