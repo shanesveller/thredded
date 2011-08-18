67bd35afaa148af629b7a53c6575d9ee9d130ae0
joel <joel.oliveira@gmail.com>
2011-08-18 12:34:50 -0400

    Adding a sequence for the title field in the messageboard factory

--- spec/factories/messageboards.rb
+++ spec/factories/messageboards.rb
@@ -1,4 +1,5 @@
 Factory.sequence(:messageboard_name) {|n| "messageboard#{n}" }
+Factory.sequence(:messageboard_title) {|n| "Messageboard #{n}" }
 
 Factory.define :messageboard do |f|
   f.name                { Factory.next :messageboard_name }
@@ -6,4 +7,5 @@ Factory.define :messageboard do |f|
   f.theme               'default'
   f.security            'public'
   f.posting_permission  'anonymous'
+  f.title               { Factory.next :messageboard_title }
 end
