c11cc7b6e23c175b52dd8378c790dbaeb9955dfa
joel <joel.oliveira@gmail.com>
2011-08-21 13:41:01 -0400

    cleaning up paths to a state that makes a little more sense and looks better

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -7,38 +7,40 @@ module NavigationHelpers
   #
   def path_to(page_name)
     case page_name
+
     when /the homepage/
       '/'
-    when /a "([^\"]+)" messageboard/i
-      m = Factory(:messageboard, :security => $1, :site => @site)
-      m.topics << Factory(:topic, :user => User.last)
-      site_messageboard_topics_path(@site, m)
+
+    when /the messageboard "([^\"]+)"/i
+      messageboard = Messageboard.find_by_name($1)
+      site_messageboard_topics_path(@site.slug, messageboard)
+
     when /the add a new thread page for "([^\"]+)"/i
-      m = Messageboard.where(:name =>$1).first
-      new_site_messageboard_topic_path(@site.slug, m)
+      messageboard = Messageboard.find_by_name($1)
+      new_site_messageboard_topic_path(@site.slug, messageboard)
+
     when /the topic listing page/i
-      m = @site.messageboards.first
-      site_messageboard_topics_path(@site.slug, m)
+      messageboard = @site.messageboards.first
+      site_messageboard_topics_path(@site.slug, messageboard)
+
     when /edit the latest thread/i
-      m = Messageboard.first
-      t = m.topics.latest.first
-      edit_site_messageboard_topic_path(@site.slug, m, t)
+      messageboard = Messageboard.first
+      topic = messageboard.topics.latest.first
+      edit_site_messageboard_topic_path(@site.slug, messageboard, topic)
+
     when /the most recently updated thread on "([^\"]+)"/i
-      m = Messageboard.where(:name =>$1).first
-      t = m.topics.latest.first
-      site_messageboard_topic_path(@site.slug, m, t)
+      messageboard = Messageboard.find_by_name($1)
+      topic = messageboard.topics.latest.first
+      site_messageboard_topic_path(@site.slug, messageboard, topic)
+
     when /the sign up page/i
       new_user_registration_path( THREDDED[:default_site] )
+
     when /the sign in page/i
       new_user_session_path( THREDDED[:default_site] )
+
     when /the password reset request page/i
       new_user_password_path( THREDDED[:default_site] )
-
-    # Add more mappings here.
-    # Here is an example that pulls values out of the Regexp:
-    #
-    #   when /^(.*)'s profile page$/i
-    #     user_profile_path(User.find_by_login($1))
 
     else
       begin
