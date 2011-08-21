ed6904fef9da51b46c0802c8c444dc2d194741b0
joel <joel.oliveira@gmail.com>
2011-08-21 13:43:05 -0400

    changing where user is sent to on AccessDenied exceptions - from root_url to site_home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
-    redirect_to root_url
+    redirect_to site_home
   end
 
   private
