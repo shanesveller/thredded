1b30d971493a8a353442a6fa295085b17182b50d
joel <joel.oliveira@gmail.com>
2011-12-05 23:46:16 -0500

    deleting shit from sites model and controller

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,14 +1,3 @@
 class SitesController < ApplicationController
   load_and_authorize_resource
-
-  def index
-    @sites = Site.all
-    render :layout => 'home'
-  end
-
-  def show
-    @site = params[:site_id].present? ? Site.find_by_slug(params[:site_id]) : Site.first
-    @messageboards = @site.messageboards
-  end
-
 end
