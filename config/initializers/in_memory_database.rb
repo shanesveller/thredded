f08d26d39530dc317a50ca6a4af67642cae7cca2
joel <joel.oliveira@gmail.com>
2011-12-07 00:09:59 -0500

    In memory db stuff works with rspec.  Hosed w/Cuke.  Commenting out for now.

--- config/initializers/in_memory_database.rb
+++ config/initializers/in_memory_database.rb
@@ -1,12 +1,12 @@
-def in_memory_database?
-  Rails.env == "test" and 
-  ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter || 
-  ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and 
-  Rails.configuration.database_configuration['test']['database'] == ':memory:'
-end
-
-if in_memory_database?
-  ActiveRecord::Schema.verbose = false
-  load "#{Rails.root}/db/schema.rb"
-end
-
+# def in_memory_database?
+#   Rails.env == "test" and 
+#   ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLiteAdapter || 
+#   ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter and 
+#   Rails.configuration.database_configuration['test']['database'] == ':memory:'
+# end
+# 
+# if in_memory_database?
+#   ActiveRecord::Schema.verbose = false
+#   load "#{Rails.root}/db/schema.rb"
+# end
+# 
