1c92ddcb27b0a20869fff2043aa3a79b83f42aca
joel <joel.oliveira@gmail.com>
2011-12-06 22:58:12 -0500

    remove unnecessary helpers

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,13 +1,3 @@
 module TopicsHelper
 
-  def link_for_create_topic(site, messageboard, topic)
-    path = create_site_messageboard_topic_path(site.slug, messageboard.name, topic)
-    path.gsub!("#{site.slug}/", '') unless %w{test}.include?( Rails.env )
-  end
-
-  def link_for_create_topic_post(site, messageboard, topic, post)
-    path = create_site_messageboard_topic_post_path(site.slug, messageboard.name, topic, post)
-    path.gsub!("#{site.slug}/", '') unless %w{test}.include?( Rails.env )
-  end
-
 end
