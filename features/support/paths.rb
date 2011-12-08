381969ed01c01e4c192714204e02424d71358fbe
joel <joel.oliveira@gmail.com>
2011-12-07 22:55:51 -0500

    changing paths to urls and specifying the :host url

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,34 +13,34 @@ module NavigationHelpers
 
     when /the messageboard "([^\"]+)"/i
       messageboard = @site.messageboards.find_by_name($1)
-      messageboard_path(messageboard)
+      messageboard_url(messageboard, :host => @site.cached_domain)
 
     when /the add a new thread page for "([^\"]+)"/i
       messageboard = @site.messageboards.find_by_name($1)
-      new_messageboard_topic_path(messageboard)
+      new_messageboard_topic_url(messageboard, :host => @site.cached_domain)
 
     when /the topic listing page/i
       messageboard = @site.messageboards.first
-      messageboard_topics_path(messageboard)
+      messageboard_topics_url(messageboard, :host => @site.cached_domain)
 
     when /edit the latest thread/i
       messageboard = Messageboard.first
       topic = messageboard.topics.latest.first
-      edit_messageboard_topic_path(messageboard, topic)
+      edit_messageboard_topic_url(messageboard, topic, :host => @site.cached_domain)
 
     when /the most recently updated thread on "([^\"]+)"/i
       messageboard = @site.messageboards.find_by_name($1)
       topic = messageboard.topics.latest.first
-      messageboard_topic_path(messageboard, topic)
+      messageboard_topic_url(messageboard, topic, :host => @site.cached_domain)
 
     when /the sign up page/i
-      new_user_registration_path( THREDDED[:default_site] )
+      new_user_registration_url( :host => @site.cached_domain )
 
     when /the sign in page/i
-      new_user_session_path( THREDDED[:default_site] )
+      new_user_session_url( :host => @site.cached_domain )
 
     when /the password reset request page/i
-      new_user_password_path( THREDDED[:default_site] )
+      new_user_password_url( :host => @site.cached_domain )
 
     else
       begin
