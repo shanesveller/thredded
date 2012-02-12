b362f7a5077946f277aa6e77512d6d9bbf72dace
Joel Oliveira <joel.oliveira@gmail.com>
2012-02-12 18:25:09 -0500

    uploading in topics#show

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,11 @@
 class Attachment < ActiveRecord::Base
   belongs_to :post
+  validates_presence_of :attachment
   attr_accessible :attachment
   mount_uploader :attachment, AttachmentUploader
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
 end
