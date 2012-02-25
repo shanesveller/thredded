b637ebaca2769fff3b5bbe3269781587006e8b59
Joel Oliveira <joel.oliveira@gmail.com>
2012-02-24 23:53:33 -0500

    adding a small monkey-patching for Array in order to split single array into chunks.  tableish is gone so had to roll this together

--- features/support/thredded.rb
+++ features/support/thredded.rb
@@ -13,3 +13,49 @@ end
 #     @after_conf = true
 #   end
 # end
+#
+class Array
+
+ def collect_every(n, fill=false, offset = 0)
+
+  if block_given?
+     while  offset < size
+          ret = []
+
+          if fill
+             n.times do |x| 
+                  if offset + x > size - 1 then ret << nil 
+                  else  ret << self[offset + x] end
+             end
+          else
+             n.times { |x| ret << self[offset + x] unless offset + x > size - 1}
+          end 
+
+          offset += n
+          yield ret
+          ret = nil
+     end   
+
+  else
+
+   ret = []
+   while  offset < size
+     ret << []
+     if fill
+         n.times do |x|  
+            if offset + x > size - 1 then ret.last << nil
+            else ret.last << self[offset + x]; end
+         end
+     else
+         n.times { |x| ret.last << self[offset + x] unless offset + x > size - 1 }
+     end
+
+     offset += n
+   end
+   return ret
+
+  end 
+
+ end
+end
+
