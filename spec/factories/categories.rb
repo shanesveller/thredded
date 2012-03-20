f2a66746a250dd3d1653944e50bc00ef5a1cee41
Shaun Butler <shaun.butler@gmail.com>
2012-03-20 11:29:49 -0400

    added a couple simple tests for categories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,4 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
-Factory.define :category do |f|
+Factory.define :category do |c|
+  c.name "Test Cat"
+  c.description "A test category"
+  c.association :messageboard
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do
+    @user = Factory(:user, :name => "categorytest")
+    @messageboard = Factory(:messageboard)
+    @topic  = Factory(:topic, :messageboard => @messageboard)
+    @category = Factory(:category, :messageboard => @messageboard)
+  end
+
+  it "should allow nil category_id" do
+    @topic.category_id = nil
+    @topic.should be_valid
+  end
+
+  it "should allow a category" do
+    @topic.category = @category
+    @topic.save
+    @topic.category_id.should_not == nil
+  end
+
 end
