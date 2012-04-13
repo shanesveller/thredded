5dd13ab1605912e2e85e4b9dae39d8df520e0f67
Joel Oliveira <joel.oliveira@gmail.com>
2012-04-13 15:52:58 -0400

    Removing User index and edit.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,9 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def show
     @user = User.find_by_name(params[:id])
   end
 
-  def index
-    @users = User.all
-  end
-  
-  def edit
-    @user = User.find(params[:id])
-  end
-
 end
+
