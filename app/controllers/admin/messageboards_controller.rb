6bdb208b352c89bc290f2f5a1acf99cd2bcaccd7
Joel Oliveira <joel@thoughtbot.com>
2012-04-24 19:47:13 -0500

    [#21] Little bit of cleanup.

--- app/controllers/admin/messageboards_controller.rb
+++ app/controllers/admin/messageboards_controller.rb
@@ -1,19 +1,18 @@
 class Admin::MessageboardsController < ApplicationController
-  load_and_authorize_resource
+
+  load_and_authorize_resource :find_by => :name
   before_filter :messageboard, :only => :show
-  
+
   def index
     redirect_to new_user_session_url(:host => site.cached_domain) unless can? :read, site
   end
 
   def edit
-    @messageboard = Messageboard.find(params[:id])
   end
 
   def update
     messageboard.update_attributes(params[:messageboard])
     redirect_to edit_admin_site_messageboard_path(site.id, messageboard.id)
   end
-  # ======================================
 
 end
