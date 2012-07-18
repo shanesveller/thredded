a8bf76ddd220f5658d33eb031567613518280c63
Shaun Butler <shaun.butler@gmail.com>
2012-07-18 18:34:24 +0300

    #46 adds / removes tags for images to post area as attachments are added

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function() {
+
   // timestamps
   adjust_timestamps();
   jQuery.timeago.settings.allowFuture = true;
   jQuery("abbr.timeago").timeago();
+
   // chosen
   jQuery("#topics_new #topic_user_id").chosen();
+
   // drawer under the topic field
   jQuery('.handle a').bind('click', function(){
     if (jQuery('#attributes').is(':hidden'))
@@ -19,6 +22,45 @@ jQuery(document).ready(function() {
     }
     return false;
   });
+
+  // check if we're on new topic / post page
+  var form_new_topic = jQuery('form#new_topic,form#new_post');
+  if (form_new_topic.length > 0) {
+    // got a new attachment added, now we listen for that onchange
+    form_new_topic.bind('nested:fieldAdded:attachments', function(e) {
+
+      // what img # we up to?
+      var seq_id = e.field.parent().find('.tag_id').length + 1;
+
+      // to keep track of added images
+      e.field.addClass('tag_id');
+      e.field.attr('img-seq', seq_id);
+
+      // bind on change, grab the ID from the attr then add tag
+      e.field.bind('change', function(e) {
+        var content = jQuery('textarea').val();
+        var id =  jQuery(this).attr('img-seq');
+        jQuery('textarea').val( content + '[t:img=' + id + ']' );
+      });
+
+    });
+
+    // removed an attachment field
+    form_new_topic.bind('nested:fieldRemoved', function(e) {
+      // remove tracker class
+      e.field.removeClass('tag_id');
+      // get id from field
+      var id = e.field.attr('img-seq');
+      // get contents
+      var content = jQuery('textarea').val();
+      // replace tag and surrounding spaces with 1 space
+      var tag_regex = new RegExp('\\s?\\[t:img=' + id + '\\]\\s?');
+      var new_text = content.replace(tag_regex, ' ');
+      new_text = adjust_tags(new_text, id);
+      jQuery('textarea').val( new_text );
+    });
+  }  // end if (form_new_topic)
+
   // chaves setup
   topic_bindings = [
     ['shift+t', 'New Topic', function(){ window.location.href = $('a:contains("Create a New Topic")').attr('href'); }]
@@ -65,4 +107,35 @@ function adjust_timestamps () {
     timestamp += offset;
     jQuery(this).attr('title', timestamp);
   });
+}
+
+function adjust_tags(content, start) {
+  // find tags
+  var tags = content.match(/(\[t:img=\d+\])/g);
+
+  if (tags) {
+    for(var index in tags) {
+      var tag = tags[index];
+      var tag_num = tag.match(/(\d+)/)[0];
+
+      // decrement if greater than starting tag
+      if (tag_num > start) {
+        var replace_with = tag.replace(tag_num, tag_num - 1);
+        content = content.replace(tag, replace_with);
+      }
+
+    }
+
+    // adjust attribute on elements img-seq
+    var elements = jQuery('.tag_id');
+    for(var i = 0; i < elements.length; i++) {
+      var element = jQuery(elements[i]);
+      var img_seq = element.attr('img-seq');
+      if ( img_seq > start ) {
+        element.attr('img-seq', img_seq - 1);
+      }
+    }
+  }
+
+  return content;
 }
