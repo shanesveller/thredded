db9f6f85af339ae62b70947e2bc9ab014b342c80
Joel Oliveira <joel@thoughtbot.com>
2012-12-19 09:24:29 -0500

    Pulling youtube image into app

--- config/initializers/pseudohelp.rb
+++ config/initializers/pseudohelp.rb
@@ -28,7 +28,7 @@ Pseudohelp.configure do |config|
       videos: {
         'Youtube' => {
           'input' => '[youtube]http://www.youtube.com/watch?v=123456[/youtube]',
-          'output' => '<img src="//www.skibowl.com/winter/sites/all/themes/winter-2012/images/youtube_player.jpg" />'
+          'output' => '<img src="/assets/pseudohelp/youtube_player.jpg" />'
         }
       }
     },
