8448a1331d9aaa1956089531f49bfc4985dad6a0
Joel Oliveira <joel@thoughtbot.com>
2012-12-23 22:56:20 -0500

    Fix bug in rendering @usernames

--- lib/at_users.rb
+++ lib/at_users.rb
@@ -2,9 +2,11 @@ class AtUsers
   def self.render(content, messageboard)
     at_names = AtNotificationExtractor.new(content).extract
     members = messageboard.members_from_list(at_names)
+
     members.each { |member|
-      content.gsub!("@#{member}", %Q{<a href="/users/#{member}">@#{member}</a>})
+      content.gsub!("@#{member.name}", %Q{<a href="/users/#{member.name}">@#{member.name}</a>})
     }
+
     content
   end
 end
