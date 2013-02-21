3b39ae6ce87b6bd0b74a1241214bc7b727c42509
Shaun Butler <shaun.butler@gmail.com>
2013-02-21 00:12:34 -0500

    Wasn't associating users with posts in my test

--- spec/lib/search_sql_builder_spec.rb
+++ spec/lib/search_sql_builder_spec.rb
@@ -4,7 +4,8 @@ describe SearchSqlBuilder, 'build' do
   it 'finds results for a simple text search' do
     messageboard = create(:messageboard)
     topic = create(:topic, messageboard: messageboard)
-    topic.posts.create!(topic: topic, messageboard: topic.messageboard, content: 'alpine spring is very refreshing')
+    topic.posts.create!(topic: topic, messageboard: topic.messageboard,
+                        content: 'alpine spring is very refreshing')
 
     search_query = ' spring '
     Topic.full_text_search(search_query, messageboard).should_not be_empty
@@ -15,11 +16,15 @@ describe SearchSqlBuilder, 'build' do
 
   it 'finds only 1 result with similar posts in different categories' do
     messageboard = create(:messageboard)
-    topic1 = create(:topic, messageboard: messageboard, title: 'A funny category topic', with_categories: 1)
-    topic2 = create(:topic, messageboard: messageboard, title: 'A beer category topic', with_categories: 1)
+    topic1 = create(:topic, messageboard: messageboard,
+                    title: 'A funny category topic', with_categories: 1)
+    topic2 = create(:topic, messageboard: messageboard,
+                    title: 'A beer category topic', with_categories: 1)
 
-    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard, content: 'spring board cats')
-    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard, content: 'alpine spring is very refreshing')
+    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard,
+                         content: 'spring board cats')
+    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard,
+                         content: 'alpine spring is very refreshing')
 
     search_query = "spring in:#{topic1.categories[0].name}"
     Topic.full_text_search(search_query, messageboard).should have(1).item
@@ -30,11 +35,15 @@ describe SearchSqlBuilder, 'build' do
     joel = create(:user, name: 'joel')
     shaun = create(:user, name: 'shaun')
 
-    topic1 = create(:topic, messageboard: messageboard, title: 'Joel made a topic', user: joel)
-    topic2 = create(:topic, messageboard: messageboard, title: 'Shaun made a topic', user: shaun)
+    topic1 = create(:topic, messageboard: messageboard,
+                    title: 'Joel made a topic', user: joel)
+    topic2 = create(:topic, messageboard: messageboard,
+                    title: 'Shaun made a topic', user: shaun)
 
-    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard, content: 'spring board cats')
-    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard, content: 'alpine spring is very refreshing')
+    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard,
+                         content: 'spring board cats', user: joel)
+    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard,
+                         content: 'alpine spring is very refreshing', user: shaun)
 
     search_query = 'spring by:shaun'
     Topic.full_text_search(search_query, messageboard).should have(1).item
@@ -42,7 +51,9 @@ describe SearchSqlBuilder, 'build' do
 
   it 'finds results with more than one specified category' do
     messageboard = create(:messageboard)
-    topic1 = create(:topic, messageboard: messageboard, title: 'topic with more than 1 category', with_categories: 2, with_posts: 2)
+    topic1 = create(:topic, messageboard: messageboard,
+                    title: 'topic with more than 1 category',
+                    with_categories: 2, with_posts: 2)
 
     search_query = "in:#{topic1.categories[0].name} in:#{topic1.categories[1].name}"
     Topic.full_text_search(search_query, messageboard).should have(1).item
@@ -53,11 +64,15 @@ describe SearchSqlBuilder, 'build' do
     joel = create(:user, name: 'joel')
     shaun = create(:user, name: 'shaun')
 
-    topic1 = create(:topic, messageboard: messageboard, title: 'Joel made a topic', user: joel)
-    topic2 = create(:topic, messageboard: messageboard, title: 'Shaun made a topic', user: shaun)
+    topic1 = create(:topic, messageboard: messageboard,
+                    title: 'Joel made a topic', user: joel)
+    topic2 = create(:topic, messageboard: messageboard,
+                    title: 'Shaun made a topic', user: shaun)
 
-    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard, content: 'spring board cats')
-    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard, content: 'alpine spring is very refreshing')
+    topic1.posts.create!(topic: topic1, messageboard: topic1.messageboard,
+                         content: 'spring board cats', user: joel)
+    topic2.posts.create!(topic: topic2, messageboard: topic2.messageboard,
+                         content: 'alpine spring is very refreshing', user: shaun)
 
     search_query = 'spring by:shaun by:joel'
     Topic.full_text_search(search_query, messageboard).should have(2).item
