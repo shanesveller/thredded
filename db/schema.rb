b555cab76538b25323c07ade41f3682ca2ca7da1
Joel Oliveira <joel.oliveira@gmail.com>
2013-06-28 22:54:13 -0400

    [JRO] Vestiges of ActiveAdmin causing issues
    Two lines in a generated migration from ActiveAdmin result in an error
    on a bare db:migrate. Commenting these out puts things back to working
    order.
    
    TravisCI should, hopefully, work now.

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "content_type"
     t.integer  "file_size"
     t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
   add_index "attachments", ["post_id"], :name => "index_attachments_on_post_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.integer  "messageboard_id"
     t.string   "name",            :null => false
     t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   add_index "categories", ["messageboard_id"], :name => "index_categories_on_messageboard_id"
@@ -59,8 +59,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.integer  "height"
     t.string   "orientation"
     t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "messageboards", :force => true do |t|
@@ -69,8 +69,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "security",           :default => "public"
     t.string   "posting_permission", :default => "anonymous"
     t.integer  "site_id",            :default => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
     t.integer  "topics_count",       :default => 0
     t.integer  "posts_count",        :default => 0
     t.string   "title"
@@ -96,8 +96,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "ip"
     t.string   "filter",          :default => "bbcode"
     t.string   "source",          :default => "web"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.integer  "topic_id"
     t.integer  "messageboard_id"
   end
@@ -119,8 +119,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
   create_table "private_users", :force => true do |t|
     t.integer  "private_topic_id"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
   add_index "private_users", ["user_id", "private_topic_id"], :name => "index_private_users_on_user_id_and_private_topic_id"
@@ -129,8 +129,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "level"
     t.integer  "user_id"
     t.integer  "messageboard_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.datetime "last_seen"
   end
 
@@ -140,8 +140,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
     t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
@@ -158,16 +158,16 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.integer  "user_id",                                 :null => false
     t.integer  "last_user_id",                            :null => false
     t.string   "title",                                   :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
     t.integer  "messageboard_id",                         :null => false
     t.string   "type"
     t.integer  "posts_count",     :default => 0
     t.string   "attribs",         :default => "[]"
     t.boolean  "sticky",          :default => false
     t.boolean  "locked"
-    t.string   "slug"
     t.string   "hash_id",                                 :null => false
+    t.string   "slug"
     t.string   "state",           :default => "approved", :null => false
   end
 
@@ -196,7 +196,6 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "email",                                 :default => "",                           :null => false
     t.string   "encrypted_password",     :limit => 128, :default => "",                           :null => false
     t.string   "reset_password_token"
-    t.string   "remember_token"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
@@ -207,8 +206,8 @@ ActiveRecord::Schema.define(:version => 20130627235006) do
     t.string   "name"
     t.boolean  "superadmin",                            :default => false,                        :null => false
     t.integer  "posts_count",                           :default => 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                                      :null => false
+    t.datetime "updated_at",                                                                      :null => false
     t.integer  "topics_count",                          :default => 0
     t.string   "time_zone",                             :default => "Eastern Time (US & Canada)"
     t.string   "post_filter"
